2cf5576c03b120790d31c8bc0f17a14822ea47b2
Daniel Ebenezer <danielebenezer.developer@gmail.com>
2021-03-22 16:34:04 +0530

    changes

---
+++ Assets/Scripts/MoveButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveButton : MonoBehaviour
+{
+    public GameObject sphere;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Block"))
+        {
+            sphere.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 7);
+        }
+    }
+}
